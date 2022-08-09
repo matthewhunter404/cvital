ed956c5674cc0814b78cef02c14b5a5a036cc621
Matthew Hunter <matthewhunter404@gmail.com>
2022-08-09 14:16:39 +0200

    Add initial user domain structure

--- domain/users/users.go
+++ domain/users/users.go
@@ -1,1 +1,29 @@
 package users
+
+type User struct {
+	FullName          string
+	EncryptedPassword string
+	EmailAddress      string
+}
+
+type CreateUserRequest struct {
+	FullName     string
+	Password     string
+	EmailAddress string
+}
+
+func CreateUser(req CreateUserRequest) error {
+
+	return nil
+}
+
+type LoginRequest struct {
+	FullName     string
+	Password     string
+	EmailAddress string
+}
+
+func Login(req LoginRequest) error {
+
+	return nil
+}
