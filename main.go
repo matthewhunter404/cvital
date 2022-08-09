dceda9b7bf1da8dbd09e1a9fb6a67059470f33c3
Matthew Hunter <matthewhunter404@gmail.com>
2022-08-09 14:16:18 +0200

    Add go-chi router with basic test responses

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello people of the world!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello people of the world!"))
+	})
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Server is running"))
+	})
+	http.ListenAndServe(":3000", r)
 }
