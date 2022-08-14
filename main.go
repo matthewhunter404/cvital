53ed2af281c28c2334bc60aaefa2dc54fd970b56
Matthew Hunter <matthewhunter404@gmail.com>
2022-08-14 17:14:11 +0200

    Add postgres docker-compose configuration

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"cvital/db"
 	"cvital/router"
+	"log"
 	"net/http"
 )
 
 func main() {
-
+	_, err := db.NewConnection(db.DatabaseConfig{
+		Host:     "cvital",
+		Port:     "5432",
+		User:     "cvital",
+		DbName:   "cvital",
+		Password: "cvital",
+		SslMode:  "false",
+	})
+	if err != nil {
+		log.Fatalf("DB connection failed")
+	}
+	log.Println("Starting Server...")
 	http.ListenAndServe(":3000", router.NewRouter())
 }
