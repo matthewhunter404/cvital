5bed525d691fb78486e1cc096bba2215f6a8fde1
Matthew Hunter <matthewhunter404@gmail.com>
2022-10-05 20:07:24 +0200

    Update React app to use createRoot rather than ReactDOM

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import App from './App'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement); // createRoot(rootElement!) if you use TypeScript
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root')
-)
+);
